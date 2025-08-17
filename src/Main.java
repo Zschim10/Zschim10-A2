1b1c01f74be03cbca30eed9e2aec0cc10f2420ae
Zschim10 <you@example.comexit>
2025-08-18 08:28:26 +1000

    Main class - demonstations exacutables with entry point and output structure

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package themPark;
+
+/**
+ * The Main class holds all the demonstrations of the theme park managment system
+ * it call in order each demonstration method from within the assignments
+ */
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //create demo driver
+        AssignmentTwo demo = new AssignmentTwo();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //These are the actual demonstrations in order
+
+        System.out.println("=== Part3: Que interface ===");
+        demo.partThree(); //remove add print visitor que
+        System.out.println();    //spacer - console readability - will do on all parts
+
+        System.out.println("=== Part4A: Ride history collection ===");
+        demo.partFourA(); //LinkedList use and iterator traversal
+        System.out.println();
+
+        System.out.println("=== Part4B: Sorting ride history ===");
+        demo.partFourB(); //Demonstrates ride history
+        System.out.println();
+
+        System.out.println("=== Part5: ride cycle ===");
+        demo.partFive(); //transefring visitors from que to ride
+        System.out.println();
+
+        System.out.println("=== Part6: export history to file ===");
+        demo.partSix(); //exasctly what it sound like
+        System.out.println();
+
+        System.out.println("=== Part7: import ride history ===");
+        demo.partSeven(); //exactly as it sounds
+        System.out.println();
     }
 }
